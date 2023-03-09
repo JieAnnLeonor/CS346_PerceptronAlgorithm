1edd09ae7374f741f35bb6a1400aa568c3b87572
Jie Ann Leonor <jieann.leonor@cit.edu>
2023-03-09 16:42:20 +0800

    Add Perceptron Algo

---
+++ PerceptronAlgorithm/SimpleBinaryClassification/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace SimpleBinaryClassification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnClassify = new System.Windows.Forms.Button();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(253, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X1: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(253, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X2: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(253, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "RESULT:";
+            // 
+            // btnClassify
+            // 
+            this.btnClassify.Location = new System.Drawing.Point(318, 139);
+            this.btnClassify.Name = "btnClassify";
+            this.btnClassify.Size = new System.Drawing.Size(75, 35);
+            this.btnClassify.TabIndex = 3;
+            this.btnClassify.Text = "Classify";
+            this.btnClassify.UseVisualStyleBackColor = true;
+            this.btnClassify.Click += new System.EventHandler(this.btnClassify_Click);
+            // 
+            // txtX1
+            // 
+            this.txtX1.Location = new System.Drawing.Point(293, 77);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(100, 20);
+            this.txtX1.TabIndex = 4;
+            // 
+            // txtX2
+            // 
+            this.txtX2.Location = new System.Drawing.Point(293, 103);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(100, 20);
+            this.txtX2.TabIndex = 5;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(358, 195);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 6;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(29, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(226, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Scatter Plot of the Training Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 254);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtX2);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.btnClassify);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Simple Binary Classification";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnClassify;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PerceptronAlgorithm/SimpleBinaryClassification/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SimpleBinaryClassification
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox pictureBox;
+        private bool pictureBoxCreated = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreatePictureBox();
+        }
+
+        private void CreatePictureBox()
+        {
+            pictureBox = new PictureBox();
+            pictureBox.Parent = this;
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.Size = new Size(200, 200);
+            pictureBox.Paint += new PaintEventHandler(pictureBox2_Paint);
+            pictureBoxCreated = true;
+        }
+
+
+
+        private void btnClassify_Click(object sender, EventArgs e)
+        {
+            if (!pictureBoxCreated)
+            {
+                CreatePictureBox();
+            }
+
+            double[] weights = new double[3];
+            Random rand = new Random();
+
+            // Initialize weights with random values between -1 and 1
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] = rand.NextDouble() * 2 - 1;
+            }
+
+            double learningRate = 0.1;
+            int iterations = 100;
+            int[][] trainingData = new int[][] {
+                new int[] {0, 0, 1},
+                new int[] {0, 1, 0},
+                new int[] {1, 0, 0},
+                new int[] {1, 1, 1}
+            };
+
+            // Train the Perceptron algorithm
+            for (int i = 0; i < iterations; i++)
+            {
+                foreach (int[] data in trainingData)
+                {
+                    int input1 = data[0];
+                    int input2 = data[1];
+                    int target = data[2];
+
+                    double output = weights[0] * input1 + weights[1] * input2 + weights[2];
+
+                    // Apply hard-limiting function
+                    if (output > 0) output = 1;
+                    else output = 0;
+
+                    double error = target - output;
+
+                    // Adjust weights
+                    weights[0] += learningRate * error * input1;
+                    weights[1] += learningRate * error * input2;
+                    weights[2] += learningRate * error;
+                }
+            }
+
+            // Classify new data
+            double testInput1 = double.Parse(txtX1.Text);
+            double testInput2 = double.Parse(txtX2.Text);
+
+            double testOutput = weights[0] * testInput1 + weights[1] * testInput2 + weights[2];
+
+            // Apply hard-limiting function
+            if (testOutput > 0) testOutput = 1;
+            else testOutput = 0;
+
+            lblResult.Text = " " + testOutput.ToString();
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            // Draw a scatter plot of the training data
+            SolidBrush brush = new SolidBrush(Color.Black);          
+            Pen pen = new Pen(brush);
+            pen.Width = 1;
+
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            e.Graphics.DrawRectangle(pen, new Rectangle(0, 0, pictureBox.Width - 1, pictureBox.Height - 1));
+
+            brush.Color = Color.Red;
+            e.Graphics.FillEllipse(brush, 50, 50, 10, 10); // Target = 1
+            brush.Color = Color.Blue;
+            e.Graphics.FillEllipse(brush, 50, 150, 10, 10); // Target = 0
+            brush.Color = Color.Blue;
+            e.Graphics.FillEllipse(brush, 150, 50, 10, 10); // Target = 0
+            brush.Color = Color.Red;
+            e.Graphics.FillEllipse(brush, 150, 150, 10, 10); // Target = 1
+        }
+    }
+}
