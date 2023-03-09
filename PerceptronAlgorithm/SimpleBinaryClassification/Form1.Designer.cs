2322dd72fba3a03dc6d9065217d2bc5de95d6a26
Jie Ann Leonor <jieann.leonor@cit.edu>
2023-03-09 16:58:07 +0800

    Add design

--- PerceptronAlgorithm/SimpleBinaryClassification/Form1.Designer.cs
+++ PerceptronAlgorithm/SimpleBinaryClassification/Form1.Designer.cs
@@ -44,6 +44,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.label1.Location = new System.Drawing.Point(253, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(34, 19);
@@ -54,6 +55,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.label2.Location = new System.Drawing.Point(253, 101);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(34, 19);
@@ -64,6 +66,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.label3.Location = new System.Drawing.Point(253, 191);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(62, 19);
@@ -72,31 +75,45 @@
             // 
             // btnClassify
             // 
+            this.btnClassify.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnClassify.FlatAppearance.BorderColor = System.Drawing.Color.CadetBlue;
+            this.btnClassify.FlatAppearance.BorderSize = 2;
+            this.btnClassify.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClassify.Font = new System.Drawing.Font("Franklin Gothic Demi", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClassify.Location = new System.Drawing.Point(318, 139);
+            this.btnClassify.Margin = new System.Windows.Forms.Padding(0);
             this.btnClassify.Name = "btnClassify";
-            this.btnClassify.Size = new System.Drawing.Size(75, 35);
+            this.btnClassify.Size = new System.Drawing.Size(75, 43);
             this.btnClassify.TabIndex = 3;
             this.btnClassify.Text = "Classify";
-            this.btnClassify.UseVisualStyleBackColor = true;
+            this.btnClassify.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClassify.UseVisualStyleBackColor = false;
             this.btnClassify.Click += new System.EventHandler(this.btnClassify_Click);
             // 
             // txtX1
             // 
+            this.txtX1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtX1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtX1.Location = new System.Drawing.Point(293, 77);
             this.txtX1.Name = "txtX1";
             this.txtX1.Size = new System.Drawing.Size(100, 20);
             this.txtX1.TabIndex = 4;
+            this.txtX1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtX2
             // 
+            this.txtX2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtX2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtX2.Location = new System.Drawing.Point(293, 103);
             this.txtX2.Name = "txtX2";
             this.txtX2.Size = new System.Drawing.Size(100, 20);
             this.txtX2.TabIndex = 5;
+            this.txtX2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // lblResult
             // 
             this.lblResult.AutoSize = true;
+            this.lblResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.lblResult.Location = new System.Drawing.Point(358, 195);
             this.lblResult.Name = "lblResult";
             this.lblResult.Size = new System.Drawing.Size(0, 13);
@@ -104,9 +121,10 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(29, 34);
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pictureBox2.Location = new System.Drawing.Point(22, 34);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.Size = new System.Drawing.Size(206, 200);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
@@ -116,6 +134,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.label4.Location = new System.Drawing.Point(15, 19);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(226, 13);
@@ -126,6 +145,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(426, 254);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox2);
@@ -149,12 +169,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button btnClassify;
         private System.Windows.Forms.TextBox txtX1;
         private System.Windows.Forms.TextBox txtX2;
         private System.Windows.Forms.Label lblResult;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label4;
+        protected System.Windows.Forms.Button btnClassify;
     }
 }
 
